7694b9c88ac861e24bd1747caeb68534cb90556e
ahushh <ahushh@gmail.com>
2020-10-12 22:23:12 +0300

    test

--- placemark.js
+++ placemark.js
@@ -14,9 +14,11 @@ function init() {
         balloonContentHeader: '<a href = "#">База отдыха Орехово</a><br>' + '<span class="description">описание</span>',
         // Зададим содержимое основной части балуна.
         balloonContentBody: `
-<a href="content/1/1.jpg" data-lightbox="roadtrip">Image #2</a>
-<a href="content/1/2.jpg" data-lightbox="roadtrip">Image #3</a>
-<a href="content/1/3.jpg" data-lightbox="roadtrip">Image #4</a>`,
+<a href="content/1/1.jpg" data-lightbox="roadtrip"><img src="content/1/1.jpg" width="100" /></a>
+<a href="content/1/2.jpg" data-lightbox="roadtrip"><img src="content/1/2.jpg" width="100" /></a>
+<a href="content/1/3.jpg" data-lightbox="roadtrip"><img src="content/1/3.jpg" width="100" /></a>
+<audio controls src="/content/1/sound.m4a"></audio>
+`,
         // Зададим содержимое нижней части балуна.
         balloonContentFooter: "футер",
         // Зададим содержимое всплывающей подсказки.
